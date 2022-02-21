99c779786b3dfaad0e91f865514e0cca3f888845
lizaemran <lizaemran56@gmail.com>
2022-02-21 06:10:22 +0500

    15. home search + responsiveness

--- client/fyp/src/components/CartHome.js
+++ client/fyp/src/components/CartHome.js
@@ -6,6 +6,7 @@ import { deleteCart } from '../redux/CartSlice';
 import {MdOutlineLocationOn} from 'react-icons/md'
 const CartHome = () => {
     const firstname = useSelector((state) => state.auth.firstname);
+    const address = useSelector((state) => state.auth.address)
     const cartItems = useSelector((state)=> state.cart.cartItems);
     const dispatch = useDispatch();
     const handleDeleted = (cId) => {
@@ -15,30 +16,31 @@ const CartHome = () => {
          }));
 	};
     return (
-        <Container >
-            <Row className='py-4'>
+        <div >
+            <Row className='py-5'>
                 <h5 className='fw-bold text-capitalize'>{firstname}</h5>
-                <h5>Cart Summary</h5>
-                <Col className='py-4'>
+                <h6>Cart Summary</h6>
+                <Col className='py-5' style={{paddingLeft:'0px'}}>
                 {cartItems !== undefined && cartItems.map((c) => 
                             <div className="cart-menu">
-                                <img src={c.image} className='w-25' alt="cart-image" style={{objectFit:"cover"}}/>
-                                <p className='fs-6'>{c.name}</p>
-                                <p className='fs-6'>x{c.countItems}</p>
-                                <p className='fs-6 px-1 fw-bold'>{c.price}</p>
+                                <img src={c.image} className='w-10 h-auto' alt="cart-image" style={{objectFit:"cover"}}/>
+                                <p className='fs-6 my-auto'>{c.name}</p>
+                                <p className='fs-6 my-auto'>x{c.countItems}</p>
+                                <p className='fs-6 px-1 fw-bold my-auto'>{c.price}</p>
                                 <span onClick={()=> handleDeleted(c.id) } style={{color:"#ef5023"}}><i className="fas fa-times"></i></span>
                             </div>
                         )
                 }
                 
                 </Col>
+                <hr />
                 <div>
                     Your Delivery Address
-                    <p><MdOutlineLocationOn className='fs-5' style={{color:"#ef5023"}}/>House no...</p>
+                    <p><MdOutlineLocationOn className='fs-5' style={{color:"#ef5023"}}/>{address}</p>
                 </div>
                 <Link to='/Cart'><Button className='m-4' style={{background:"#ef5023", border:"none", outline:"none"}}>Go To Cart</Button></Link>
             </Row>
-        </Container>
+        </div>
     )
 }
 
--- client/fyp/src/pages/Home.jsx
+++ client/fyp/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState, useContext} from 'react'
-import pizza from '../img/pizza.svg';
-import burger from '../img/burger.svg';
-import fries from '../img/fries.svg';
-import soda from '../img/soda.svg';
+// import pizza from '../img/pizza.svg';
+// import burger from '../img/burger.svg';
+// import fries from '../img/fries.svg';
+// import soda from '../img/soda.svg';
 import Nav from '../components/Nav';
 import Card from '../components/Card';
 import { useSelector , useDispatch} from 'react-redux';
@@ -20,9 +20,9 @@ import { getUserAsync, getAdminAsync } from '../redux/auth';
 import jwt_decode from "jwt-decode";
 import Cart from './Cart';
 import CartHome from '../components/CartHome';
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import 'swiper/swiper-bundle.min.css'
+// import 'swiper/swiper.min.css'
 const Home = ({pId, setPId, isEditP, setIsEditP, search, setSearch}) => {
     const {cartItems, total} = useSelector((state)=> state.cart);
     const cart = useSelector((state)=> state.cart);
@@ -33,24 +33,8 @@ const Home = ({pId, setPId, isEditP, setIsEditP, search, setSearch}) => {
     const dispatch = useDispatch();
 	const restaurants = useSelector((state)=> state.restaurants.restaurants);
 	const token = useSelector((state)=> state.auth.token);
-    const [searched, setSearched] = useState([]);
+    const [searched, setSearched] = useState(restaurants);
     const [rId, setRId] = useState();
-    
-    useEffect(()=> {
-		dispatch(getRestaurantsAsync());
-        var decoded = jwt_decode(token);
-        // console.log(decoded);
-        if(decoded.isAdmin === true){
-            dispatch(getAdminAsync());
-        }
-        else if(decoded.isUser === true){
-            dispatch(getUserAsync());
-        }
-        else{
-            window.location.href='/';
-        }
-        
-	},[dispatch]);
    
     useEffect(() => {
         if(total === 0){
@@ -68,24 +52,29 @@ const Home = ({pId, setPId, isEditP, setIsEditP, search, setSearch}) => {
                 JSON.parse(localStorage.getItem("cart")),
             ))
         }
-   
+        dispatch(getRestaurantsAsync());
+        var decoded = jwt_decode(token);
+        // console.log(decoded);
+        if(decoded.isAdmin === true){
+            dispatch(getAdminAsync());
+        }
+        else if(decoded.isUser === true){
+            dispatch(getUserAsync());
+        }
+        else{
+            window.location.href='/';
+        }
         
     }, []);
     useEffect(()=>{
-        if(search!= null && search.length>2){
-        setSearched(restaurants.filter((r)=> {
-            return r.name.toLowerCase().includes(search.toLowerCase());
-        }
-        ))
+        if(search!= null && search.length>1){
+        setSearched(restaurants.filter((r)=> r.name.toLowerCase().includes(search.toLowerCase())));
     }
     else{
         setSearched(restaurants);
     }
-    },[search, restaurants])
+    },[search.length > 1])
 
-    useEffect(()=> {
-        setSearched(restaurants);
-    },[restaurants])
    const userType = useSelector((state)=> state.auth?.user_type)
     return (
         <>
@@ -93,7 +82,7 @@ const Home = ({pId, setPId, isEditP, setIsEditP, search, setSearch}) => {
             <Col xl={1} lg={1} md={1} sm={12} xs={12} >
             <SideNav />
             </Col>
-            <Col >
+            <Col style={{paddingLeft:'40px'}}>
             <Nav search={search} setSearch={setSearch} />
             {userType === 'admin' && <i onClick={()=>{setIsAddR(true)}} id="addRestaurant"className="fas fa-plus"></i>}
             {/* <img id="pizza"src={pizza} alt="pizza"/>
@@ -102,11 +91,10 @@ const Home = ({pId, setPId, isEditP, setIsEditP, search, setSearch}) => {
             <img id="soda" src={soda} alt="soda"/> */}
             <Container  className="home-container">
             <Row>
-            <Swiper className="mySwiper " slidesPerView={3} spaceBetween={10}  breakpoints = {{ 300 : {slidesPerView : 1} ,499 : {slidesPerView : 1} , 800 : {slidesPerView : 2}, 1024: {slidesPerView : 3}}}>
-
+            {/* <Swiper className="mySwiper " slidesPerView={3} spaceBetween={10}  breakpoints = {{ 300 : {slidesPerView : 1} ,499 : {slidesPerView : 1} , 800 : {slidesPerView : 2}, 1024: {slidesPerView : 3}}}> */}
             {searched?.map((restaurant) => (
-                <Col xl={6} lg={6} md={6} sm={12} xs={12} key={restaurant._id}>
-                <SwiperSlide  style={{width: "426px", height:"410px"}}>
+                <div key={restaurant._id}>
+                {/* <SwiperSlide  style={{width: "426px", height:"410px"}}> */}
 				<Card  
                 key={restaurant._id} 
                 id={restaurant._id} 
@@ -122,24 +110,25 @@ const Home = ({pId, setPId, isEditP, setIsEditP, search, setSearch}) => {
                 setIsEditP={setIsEditP}
                 setIsEditStatus={setIsEditStatus}
                 />
-                </SwiperSlide>
-                </Col>
+                {/* </SwiperSlide> */}
+                </div>
 			))}
-            </Swiper>
+            {/* </Swiper> */}
             </Row>
             </Container>
+        
+            </Col>
+            {total > 0 && 
+            <Col  xl={2} lg={2} md={2} sm={12} xs={12} className='bg-light' style={{marginLeft:'0px'}}>
+                <CartHome />
+            </Col>}
                 {isAddR && <FormPopUp title="Add Restaurant" setIsOpen={setIsAddR}><AddRestaurant /></FormPopUp>}
                 {isEdit && <FormPopUp title="Update Restaurant" setIsOpen={setIsEdit}><UpdateRestaurant rId={rId} /></FormPopUp>}
                 {isAdd && <FormPopUp title="Add Product" setIsOpen={setIsAdd}><AddProduct rId={rId}/></FormPopUp> }
                 {isEditP && <FormPopUp title="UpdateProduct" setIsOpen={setIsEditP}><UpdateProduct pId={pId} setPId={setPId}/></FormPopUp>}
                 {isEditStatus && <FormPopUp title="Update Status" setIsOpen={setIsEditStatus}><UpdateStatus rId={rId} status={restaurants.status}/></FormPopUp>}
-        
-            </Col>
-            {total > 0 && 
-            <Col  xl={2} lg={2} md={2} sm={12} xs={12} className='bg-light'>
-                <CartHome />
-            </Col>}
             </Row>
+               
           </>
     )
 }
